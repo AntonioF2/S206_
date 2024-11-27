d52b9ee00376eed7c7b78ea799ff16c1db66d44e
AntonioF2 <antonio.faria.cn@gmail.com>
2024-11-26 21:03:38 -0300

    final prova cypress
    terminei

--- ProvaLabCypress/cypress.config.js
+++ ProvaLabCypress/cypress.config.js
@@ -1,7 +1,10 @@
-module.exports = {
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      requestAnimationFrame('cypress-mochawesome-reporter/plugin')(on)// implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
-};
+});
